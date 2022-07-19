c0a1e30ce5e5102e1777eb88cc410f82213d9aa1
danida21 <danidanih21@gmail.com>
2022-07-19 16:50:41 +0300

    update

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
+import data from './data/data';
+
 function App() {
   return (
     <div>
       <header>
         <a href="/">Gebeya</a>
       </header>
-      <main>Products Lists</main>
+      <main>
+        <h2>Featured Products</h2>
+        <div className="products">
+          {data.products.map((product) => (
+            <div className="product" key={product.slug}>
+              <a href={`/product/${product.slug}`}>
+                <img src={product.image} alt={product.name} />
+              </a>
+              <div className="product-info"></div>
+              <a href={`/product/${product.slug}`}>
+                <p>{product.name}</p>
+              </a>
+              <p>
+                <strong>${product.price}</strong>
+              </p>
+              <button>Add to Cart</button>
+            </div>
+          ))}
+        </div>
+      </main>
     </div>
   );
 }
