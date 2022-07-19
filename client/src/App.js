7b2ff598a4ea6cab741e638b79f26b56ded57980
danida21 <danidanih21@gmail.com>
2022-07-19 15:11:00 +0300

    gebeya-ecommerce-web-app

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Gebeya</a>
+      </header>
+      <main>Products Lists</main>
+    </div>
+  );
+}
+
+export default App;
