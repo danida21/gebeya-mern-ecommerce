139ede44f18c0d74ceadbc672dc7252af0029a91
danida21 <danidanih21@gmail.com>
2022-07-26 15:00:19 +0300

    heroku publish

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import data from './data.js';
+import path from 'path';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import seedRouter from './routes/seedRoutes.js';
@@ -33,6 +33,11 @@ app.use('/api/products', productRouter);
 app.use('/api/users', userRouter);
 app.use('/api/orders', orderRouter);
 
+const _dirname = path.resolve();
+app.use(express.static(path.join(_dirname, '/client/build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(_dirname, '/client/build/index.html'));
+});
 app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
