ce2d9fe625ba0debbdf5fc194cc3f2d6c579aaf5
danida21 <danidanih21@gmail.com>
2022-07-27 16:40:32 +0300

    adding products

--- client/src/App.js
+++ client/src/App.js
@@ -79,7 +79,10 @@ function App() {
                 <SearchBox />
                 <Nav className="me-auto w-100 justify-content-end">
                   <Link to="/cart" className="nav-link">
-                    Cart
+                    <i
+                      class="fas fa-shopping-cart"
+                      // style={{ color: 'white' }}
+                    ></i>
                     {cart.cartItems.length > 0 && (
                       <Badge pill bg="danger">
                         {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
